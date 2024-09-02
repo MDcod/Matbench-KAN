1a41ab254c38b68b30a27590cc9eb10011f43bbd
milkov [vm-rmtdev-01] <milkov.de@skbkontur.ru>
2024-09-02 15:01:35 +0000

    regression (composition & structure)

--- start.py
+++ start.py
@@ -20,70 +20,89 @@ from matminer.featurizers.composition.element import ElementFraction
 from pymatgen.core.structure import Structure
 
 import torch
+import json
 
 class MYKAN:
 
-    def train_and_validate(self, train_inputs, train_outputs, test_inputs, test_outputs):
+  def train_and_validate(self, train_inputs, train_outputs, test_inputs, test_outputs):
 
-      #  X = self.transformInput(train_inputs)
-      #  y = list(train_outputs);
+      y_train = train_outputs
+      y_test = test_outputs
+      X_train_tuple = self.transformInput(train_inputs)
+      X_test_tuple =  self.transformInput(test_inputs)
+      
+      if (len(X_train_tuple[0][0]) == len(X_test_tuple[0][0])):
+        print('Одинаковые размерности! Берём сокращенный data frame')
+        X_train = X_train_tuple[0]
+        X_test =  X_test_tuple[0]
+        self.fullInputDf = False
+      else:
+        print('Разные размерности! Берём полный data frame')
+        X_train = X_train_tuple[1]
+        X_test =  X_test_tuple[1]
+        self.fullInputDf = True
 
-      #  X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+      self.kan = KAN(width=[X_train.shape[1], 9, 1], grid=5, k=3, seed=0)
 
-       X_train = self.transformInput(train_inputs)
-       X_test =  self.transformInput(test_inputs)
-       y_train = train_outputs
-       y_test = test_outputs
+      print(len(X_train[0]))
+      print(len(X_test[0]))
 
-       self.kan = KAN(width=[X_train.shape[1], [65, 26],  1], grid=5, k=3, seed=0)
+      dataset = {
+        'train_input': torch.tensor(X_train, dtype=torch.float32),
+        'train_label': torch.tensor(y_train, dtype=torch.float32).unsqueeze(1),
+        'test_input': torch.tensor(X_test, dtype=torch.float32),
+        'test_label': torch.tensor(y_test, dtype=torch.float32).unsqueeze(1)
+      }
 
-       print(len(X_train[0]))
-       print(len(X_test[0]))
+      self.kan(dataset['train_input'])
+    #  self.kan.plot()
 
-       dataset = {
-          'train_input': torch.tensor(X_train, dtype=torch.float32),
-          'train_label': torch.tensor(y_train, dtype=torch.float32).unsqueeze(1),
-          'test_input': torch.tensor(X_test, dtype=torch.float32),
-          'test_label': torch.tensor(y_test, dtype=torch.float32).unsqueeze(1)
-       }
+      self.kan.fit(dataset, steps=20, lamb=0.001, lamb_entropy=2.7876383801674827)
+    #  self.kan.plot()
 
-       self.kan(dataset['train_input'])
-      #  self.kan.plot()
+  def predict(self, test_inputs):
+    test_input_tuple = self.transformInput(test_inputs)
+    tensorInput = torch.tensor(test_input_tuple[1] if self.fullInputDf else test_input_tuple[0], dtype=torch.float32)
+    self.predictInput = tensorInput
+    pred = self.kan(tensorInput).detach().numpy()
+    return pred
 
-       self.kan.fit(dataset, opt="LBFGS", steps=20, lamb=0.01, lamb_entropy=2.7876383801674827)
-      #  self.kan.plot()
+  def transformInput(self, inputs):
+    if (type(inputs[0]) == str):
+      stc = StrToComposition(overwrite_data=True)
+      df = stc.featurize_dataframe(inputs.to_frame(), "composition")
 
-    def predict(self, test_inputs):
-      tensorInput = torch.tensor(self.transformInput(test_inputs), dtype=torch.float32)
-      self.predictInput = tensorInput
-      pred = self.kan(tensorInput).detach().numpy()
-      return pred
+      featurizer = ElementFraction()
+      df = featurizer.featurize_dataframe(df, col_id='composition')
+      collapsedDf = df.loc[:, (df != 0).any(axis=0)]
+      
+      fullValues = df[df.columns[1:]].values
+      collapsedValues = collapsedDf[collapsedDf.columns[1:]].values
+      
+      return (collapsedValues, fullValues);
 
-    def transformInput(self, inputs):
-      if (type(inputs[0]) == str):
-        stc = StrToComposition(overwrite_data=True)
-        df = stc.featurize_dataframe(inputs.to_frame(), "composition")
+    if (type(inputs[0]) == Structure):
+      stc = StructureToComposition(overwrite_data=True)
+      df = stc.featurize_dataframe(inputs.to_frame(), "structure")
 
-        featurizer = ElementFraction()
-        df = featurizer.featurize_dataframe(df, col_id='composition')
-        df = df.loc[:, (df != 0).any(axis=0)]
-        self.df = df
+      featurizer = ElementFraction()
+      df = featurizer.featurize_dataframe(df, col_id='composition')
+      collapsedDf = df.loc[:, (df != 0).any(axis=0)] # тут это не работает потому что получаются разные размерности в тестовых и тренировочных данных
 
-        return df[df.columns[1:]].values;
+      fullValues = df[df.columns[2:]].values
+      collapsedValues = collapsedDf[collapsedDf.columns[2:]].values
+      
+      return (collapsedValues, fullValues);
 
-      if (type(inputs[0]) == Structure):
-        stc = StructureToComposition(overwrite_data=True)
-        df = stc.featurize_dataframe(train_inputs.to_frame(), "structure")
-
-        featurizer = ElementFraction()
-        df = featurizer.featurize_dataframe(df, col_id='composition')
-
-        return df[df.columns[1:]].values;
-
-mb = MatbenchBenchmark(autoload=False, subset=['matbench_steels'])
+set='matbench_jdft2d'
+mb = MatbenchBenchmark(autoload=False)
 model = MYKAN()
 
 for task in mb.tasks:
+    if task.metadata["task_type"] == "classification":
+      print(f'task {task.dataset_name} is classification, skip it')
+      continue
+    
     task.load()
     for fold in task.folds:
 
@@ -104,6 +123,16 @@ for task in mb.tasks:
       # Record your data!
       task.record(fold, predictions)
     
+    print(task.is_recorded)
+    print(json.dumps(task.scores, indent=4))
     print(task.scores.mae.mean)
+    
+    with open(f'benches/{task.dataset_name}-result.txt', 'w', encoding='utf-8') as file:
+      file.write(str(task.is_recorded) + '\n')
+      file.write(json.dumps(task.scores, indent=4) + '\n')
+      file.write(str(task.scores.mae.mean) + '\n')
+      
+      
 # Save your results
-mb.to_file(f'benches/my_models_benchmark-{datetime.now().strftime("%m-%d-%Y-%H-%M-%S")}.json')
+os.makedirs('benches', exist_ok = True)
+mb.to_file(f'benches/KAN_benchmark-{datetime.now().strftime("%m-%d-%Y-%H-%M-%S")}.json')
